4160c256eb7088933d533cf75646ecad3b2c4bd5
Sergio Martinez Lu <sergiomlu@protonmail.com>
2025-03-09 21:53:14 -0600

    Added test to verify behavior when logging into SimplePractice with invalid credentials

---
+++ spec/spec_helper.rb
@@ -0,0 +1,22 @@
+# frozen-string-literal: true
+require 'rspec'
+require 'capybara/rspec'
+require 'capybara/dsl'
+require 'selenium-webdriver'
+
+pages  = File.join(Dir.pwd, 'spec/pages/**/*.rb')
+Dir.glob(pages).each { |file| require file }
+
+Capybara.register_driver :firefox do |app|
+  Capybara::Selenium::Driver.new(app, browser: :firefox)
+end
+
+Capybara.default_driver = :firefox
+Capybara.app_host = 'https://secure.simplepractice.com/'
+Capybara.default_max_wait_time = 5
+
+RSpec.configure do |config|
+  config.before(:each) do
+    config.include Capybara::DSL
+  end
+end

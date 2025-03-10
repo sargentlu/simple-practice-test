1f40ebc6ca30e42f9b6d6440f4205de95aefc0cc
Sergio Martinez Lu <sergiomlu@protonmail.com>
2025-03-09 21:53:14 -0600

    Added test to verify behavior when logging into SimplePractice with invalid credentials

---
+++ spec/tests/user_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'Simple Practice Test' do
+  before(:each) do
+    visit('/')
+    @sign_in_card = SignInCard.new
+  end
+
+  it 'Returns an error if incorrect credentials' do
+    error_message = "Enter the email associated with your account and double-check"\
+      " your password. If you're still having trouble, you can reset your password."
+    @sign_in_card.login_with('incorrect@mail.com', 'password')
+    within(find('.alert-container')) do
+      expect(page).to have_content error_message
+    end
+  end
+
+end

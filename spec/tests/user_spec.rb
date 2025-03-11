9e2cf292504f6d1832c25a1381eb4426e0eba740
Sergio Martinez Lu <sergiomlu@protonmail.com>
2025-03-11 01:32:19 -0600

    Replaces INCORRECT_PASSWORD with INCORRECT_PWD on ENV

--- spec/tests/user_spec.rb
+++ spec/tests/user_spec.rb
@@ -11,7 +11,7 @@ describe 'Simple Practice Test' do
   it 'Returns an error if incorrect credentials' do
     error_message = "Enter the email associated with your account and double-check"\
       " your password. If you're still having trouble, you can reset your password."
-    @sign_in_card.login_with(ENV['INCORRECT_EMAIL'], ENV['INCORRECT_PASSWORD'])
+    @sign_in_card.login_with(ENV['INCORRECT_EMAIL'], ENV['INCORRECT_PWD'])
     within(find('.alert-container')) do
       expect(page).to have_content error_message
     end

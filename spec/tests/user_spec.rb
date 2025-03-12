d946b4a2051d26f92423aefea5da9203202c2e53
Sergio Martinez Lu <sergiomlu@protonmail.com>
2025-03-12 11:13:52 -0600

    Removed unnecessary @session argument from @sign_in_card.login_with()

--- spec/tests/user_spec.rb
+++ spec/tests/user_spec.rb
@@ -16,7 +16,7 @@ describe 'Simple Practice Test' do
   it 'Returns an error if incorrect credentials' do
     error_message = "Enter the email associated with your account and double-check"\
       " your password. If you're still having trouble, you can reset your password."
-    @sign_in_card.login_with(ENV['INCORRECT_EMAIL'], ENV['INCORRECT_PWD'], @session)
+    @sign_in_card.login_with(ENV['INCORRECT_EMAIL'], ENV['INCORRECT_PWD'])
     within(find('.alert-container')) do
       expect(page).to have_content error_message
     end
@@ -25,7 +25,7 @@ describe 'Simple Practice Test' do
   it 'Creates a client' do
     @new_client_card = NewClientCard.new
 
-    @sign_in_card.login_with(ENV['USER_EMAIL'], ENV['USER_PWD'], @session)
+    @sign_in_card.login_with(ENV['USER_EMAIL'], ENV['USER_PWD'])
 
     @new_client_card.create_client(
       ENV['CLIENT01_FIRST_NAME'],

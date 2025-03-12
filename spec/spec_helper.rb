81909976c3669722acc28b75f644be38329f9809
Sergio Martinez Lu <sergiomlu@protonmail.com>
2025-03-11 21:26:03 -0600

    Added sessions to handle different browsers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,16 +9,40 @@ require 'selenium-webdriver'
 pages  = File.join(Dir.pwd, 'spec/pages/**/*.rb')
 Dir.glob(pages).each { |file| require file }
 
-Capybara.register_driver :firefox do |app|
-  Capybara::Selenium::Driver.new(app, browser: :firefox)
-end
-
-Capybara.default_driver = :firefox
-Capybara.app_host = 'https://secure.simplepractice.com/'
-Capybara.default_max_wait_time = 5
-
 RSpec.configure do |config|
   config.before(:each) do
     config.include Capybara::DSL
   end
+end
+
+def create_web_session
+  Capybara.app_host = 'https://secure.simplepractice.com/'
+  Capybara.run_server = false
+  Capybara.default_max_wait_time = 5
+
+  if ENV['CHROME_URL']
+    Capybara.register_driver :remote do |app|
+      Capybara::Selenium::Driver.new(
+        app,
+        browser: :remote,
+        options: Selenium::WebDriver::Options.chrome,
+        url: ENV['CHROME_URL']
+      )
+    end
+    @session = Capybara::Session.new(:remote)
+  else
+    Capybara.register_driver :remote do |app|
+      Capybara::Selenium::Driver.new(
+        app,
+        browser: :firefox,
+        options: Selenium::WebDriver::Options.firefox
+      )
+    end
+    @session = Capybara::Session.new(:selenium)
+  end
+end
+
+def end_web_session(session)
+  session.reset!
+  session.driver.quit
 end
--- spec/tests/user_spec.rb
+++ spec/tests/user_spec.rb
@@ -4,14 +4,19 @@ require 'spec_helper'
 
 describe 'Simple Practice Test' do
   before(:each) do
-    visit('/')
-    @sign_in_card = SignInCard.new
+    @session = create_web_session
+    @session.visit('/')
+    @sign_in_card = SignInCard.new(@session)
+  end
+
+  after(:each) do
+    end_web_session(@session)
   end
 
   it 'Returns an error if incorrect credentials' do
     error_message = "Enter the email associated with your account and double-check"\
       " your password. If you're still having trouble, you can reset your password."
-    @sign_in_card.login_with(ENV['INCORRECT_EMAIL'], ENV['INCORRECT_PWD'])
+    @sign_in_card.login_with(ENV['INCORRECT_EMAIL'], ENV['INCORRECT_PWD'], @session)
     within(find('.alert-container')) do
       expect(page).to have_content error_message
     end
@@ -20,7 +25,7 @@ describe 'Simple Practice Test' do
   it 'Creates a client' do
     @new_client_card = NewClientCard.new
 
-    @sign_in_card.login_with(ENV['USER_EMAIL'], ENV['USER_PWD'])
+    @sign_in_card.login_with(ENV['USER_EMAIL'], ENV['USER_PWD'], @session)
 
     @new_client_card.create_client(
       ENV['CLIENT01_FIRST_NAME'],
@@ -30,11 +35,13 @@ describe 'Simple Practice Test' do
   end
 
   it 'Verifies the created client is present' do
-    @clients_page = ClientsPage.new
+    @clients_page = ClientsPage.new(@session)
+    email = ENV['USER_EMAIL']
+    password = ENV['USER_PWD']
     first_name = ENV['CLIENT01_FIRST_NAME']
     last_name = ENV['CLIENT01_LAST_NAME']
 
-    @sign_in_card.login_with(ENV['USER_EMAIL'], ENV['USER_PWD'])
+    @sign_in_card.login_with(email, password)
 
     client = @clients_page.search_client(first_name, last_name)
     expect(client['client_name']).to eq("%s %s" % [first_name, last_name])
